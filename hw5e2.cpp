27a13fd9ee0079f163a91d5fd5ee9ac13c00b61c
mkeriy <71606512+mkeriy@users.noreply.github.com>
2020-10-09 00:10:15 +0300

    Add files via upload

--- hw5e2.cpp
+++ hw5e2.cpp
@@ -13,24 +13,39 @@ class Pressure
     Pressure (double value):
     m_value(value)
     {};
+
+    ~Pressure (){};
     
     
-    
-   
-    void bar()
+    auto getter()
     {
-        m_value *= c_bar;    
-    }
-    void atm()
-    {
-        m_value *= c_atm; 
-    } 
-    void mmHg()
-    {
-        m_value *= c_mmhg;
+        return m_value;
     }
 
- static constexpr double c_bar = 100000; 
+    auto setter(double v)
+    {
+        m_value = v;
+        return m_value;
+    }
+    
+    auto bar()
+    {
+        m_value *= c_bar;
+        return m_value;    
+    }
+    auto atm()
+    {
+        m_value *= c_atm; 
+        return m_value;
+    } 
+    auto mmHg()
+    {
+        m_value *= c_mmhg;
+        return m_value;
+    }
+
+    
+    static constexpr double c_bar = 100000; 
     static constexpr double c_atm = 98066.5;
     static constexpr double c_mmhg = 133.3224;
 
@@ -49,58 +64,38 @@ enum class type
 int main()
 {
     Pressure m_value(0);
-    std::string m_type;
+    int m_type;
     type t;
     
 
     std::cout << "This program can only convert values from bar, mmHg and atmospheres to Pascals" << std::endl;
-    std::cout << "Enter the value and the pressure mesurement separated by a space." << std::endl;
+    std::cout << "Enter the value" << std::endl;
     std::cin >> m_value;
+    
+
+   std::cout << "1 - bar \n" << "2 - atm \n" << "3 - mmHg\n" << std::endl;
+   std::cout << "Enter a Number : ";
     std::cin >> m_type;
-
-    if (m_type == "bar")
-    {
-        t = static_cast <type> (1);
-    }
-    else
-    {
-          std::cout << "Wrong value of mesurement. Try again... " << std::endl;
-    }
-    if (m_type == "atm")
-    {
-        t = static_cast <type> (2);
-    }
-    else
-    {
-          std::cout << "Wrong value of mesurement. Try again... " << std::endl;
-    }
-     if (m_type == "mmHg")
-    {
-        t = static_cast <type> (3);
-    }
-    else
-    {
-          std::cout << "Wrong value of mesurement. Try again... " << std::endl;
-    }
-
+    std::cout << std::endl;
+    t = static_cast <type> (m_type);
     switch (t)
     {
         case type::bar:
-            m_value.bar();
+                m_value.setter(m_value.bar());
             break;
         case type::atm:
-            m_value.atm();
+                m_value.setter(m_value.atm());
             break;
         case type::mmHg:
-            m_value.mmHg();
+                m_value.setter(m_value.mmHg());
             break;
         default:
             std::cout << "Wrong value of mesurement. Try again... " << std::endl;
             break;
 
     }
-
-    std::cout << "Converted value: " << m_value << " Pa" << std::endl;
+   
+    std::cout << "Converted value: " << m_value.getter() << " Pa" << std::endl;
 
     return 0;
 }

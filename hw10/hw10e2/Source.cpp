43fc5ea974d287a59fb6a569b479db3a2e7527a4
mkeriy <71606512+mkeriy@users.noreply.github.com>
2020-11-13 16:43:54 +0300

    Add files via upload

---
+++ hw10/hw10e2/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+
+template <typename T>
+T* func(T arg)
+{
+	return new T(arg);
+}
+
+int main()
+{
+	double* ex = func(1.0);
+	std::cout << ex << std::endl;
+
+	return 0;
+}

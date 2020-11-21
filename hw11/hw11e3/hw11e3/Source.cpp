b271e36f7f0b611fa81c6ae330cb5178e9ee95f2
mkeriy <71606512+mkeriy@users.noreply.github.com>
2020-11-21 12:16:56 +0300

    hw

--- hw11/hw11e3/hw11e3/Source.cpp
+++ hw11/hw11e3/hw11e3/Source.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 
 
+template<typename T>
+constexpr bool is_function_v = std::is_function<T>::value;
+template<typename T>
+constexpr bool is_array_v = std::is_array<T>::value;
 
 template <typename T>
 struct decay 
 { 
 	using T1 = std::remove_reference_t<T>;
-	using type = typename std::conditional_t<std::is_array<T>::value, typename std::add_pointer_t<std::remove_extent_t<T1>>, 
-		std::conditional_t<std::is_function<T>::value, typename std::add_pointer_t<T1>, typename std::remove_const_t<T1>>>;
+	using type = typename std::conditional_t<is_array_v<T>, std::add_pointer_t<std::remove_extent_t<T1>>,
+		std::conditional_t<is_function_v<T>, std::add_pointer_t<T1>, std::remove_const_t<T1>>>;
 	
 
 };
@@ -21,7 +25,7 @@ int f() { return 0;  }
 int main()
 {
 	int a[1];
-	std::cout << std::is_same<decay<decltype(f)>::type, int*>::value;
+	std::cout << std::is_same<decay<decltype(f)>::type, int(*)()>::value;
 	std::cout << std::is_same<decay<decltype(a)>::type, int*>::value;
 	std::cout << std::is_same<decay<const int&>::type, int>::value;
 	std::cout << std::is_same<decay<int&&>::type, int>::value;
